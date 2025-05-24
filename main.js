66c64cd97285aa59179771ce9d4d0baf8266c867
ANGEL <angelwer546@gmail.com>
2025-05-23 20:16:52 -0400

    angel

--- main.js
+++ main.js
@@ -8,4 +8,13 @@ function incrementar() {
   // Actualizamos el contenido del elemento con el nuevo valor
   document.getElementById('contador').textContent = valor;
 }
+function decrementar() {
+  // Obtenemos el valor actual del contador
+  let valor = document.getElementById('contador').textContent;
+  // Convertimos el valor a un número entero
+  valor = parseInt(valor);
+  // Decrementamos el valor
+  valor--;
+  // Actualizamos el contenido del elemento con el nuevo valor
+  document.getElementById('contador').textContent = valor;
 }
